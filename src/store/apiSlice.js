4a6831b0683235e62ef0bb6412d34d9decc129d4
mostafa-moharam <mostafamoharam26@gmail.com>
2026-03-10 00:51:37 +0200

    delete notes

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -11,26 +11,6 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
-// const baseQueryWithReAuth = async (args, api, extraOptions) => {
-//   let result = await baseQuery(args, api, extraOptions);
-//   if (result?.error?.status === 403 || result?.error?.status === 401) {
-//     const res = await baseQuery(
-//       "/api/accounts/refresh-token",
-//       api,
-//       extraOptions
-//     );
-//     if (res?.data?.isSuccessed) {
-//       result = await baseQuery(args, api, extraOptions);
-//     } else {
-//       if (res.error.status === 403 || res.error.status === 401) {
-//         res.error.data.message = "Your Session has expired !";
-//       }
-//       console.log(res);
-//       return res;
-//     }
-//   }
-//   return result;
-// };
 export const apiSlice = createApi({
   baseQuery: baseQuery,
   endpoints: () => ({}),
